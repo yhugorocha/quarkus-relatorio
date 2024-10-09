679612af50da33174f6c05a1d73350d1f908ef79
Hugo Silva <jose.silva@extreme.digital>
2024-10-09 17:14:27 -0300

    feat: add repository Quotation and Opportunity

---
+++ src/main/java/io/github/yhugorocha/repository/QuotationRepository.java
@@ -0,0 +1,9 @@
+package io.github.yhugorocha.repository;
+
+import io.github.yhugorocha.entity.QuotationEntity;
+import io.quarkus.hibernate.orm.panache.PanacheRepository;
+import jakarta.enterprise.context.ApplicationScoped;
+
+@ApplicationScoped
+public class QuotationRepository implements PanacheRepository<QuotationEntity> {
+}
