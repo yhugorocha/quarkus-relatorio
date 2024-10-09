48438add28c4b134f51a965abf7a72fc338530b1
Hugo Silva <jose.silva@extreme.digital>
2024-10-09 17:12:01 -0300

    feat: initial commit

---
+++ src/test/java/io/github/yhugorocha/GreetingResourceIT.java
@@ -0,0 +1,8 @@
+package io.github.yhugorocha;
+
+import io.quarkus.test.junit.QuarkusIntegrationTest;
+
+@QuarkusIntegrationTest
+class GreetingResourceIT extends GreetingResourceTest {
+    // Execute the same tests but in packaged mode.
+}
